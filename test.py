cfa1e1a0c549a892792ecf5b2fd127d157075a1d
AndrewWu-mscience <33030035+AndrewWu-mscience@users.noreply.github.com>
2017-10-24 12:12:31 -0400

    added one more line

--- test.py
+++ test.py
@@ -15,3 +15,7 @@ df = pd.DataFrame(columns=['a', 'b', 'c'])
 df['a'], df['b'], df['c'] = range(10), range(10), range(10)
 df = spark.createDataFrame(df)
 display(df)
+
+# COMMAND ----------
+
+df.rdd.getNumPartitions()
