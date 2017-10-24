e255ab2b0883aadf743c378987c5525e43255bc2
AndrewWu-mscience <33030035+AndrewWu-mscience@users.noreply.github.com>
2017-10-24 12:10:46 -0400

    some contents are added

--- test.py
+++ test.py
@@ -1,2 +1,17 @@
 # Databricks notebook source
+import pandas as pd
+
+# COMMAND ----------
+
 '''some text'''
+
+# COMMAND ----------
+
+'''other text'''
+
+# COMMAND ----------
+
+df = pd.DataFrame(columns=['a', 'b', 'c'])
+df['a'], df['b'], df['c'] = range(10), range(10), range(10)
+df = spark.createDataFrame(df)
+display(df)
